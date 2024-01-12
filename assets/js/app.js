23349b092112d38bb17d3cf93655292b447cfbab
Agustin Dorta <yoelagustindorta@gmail.com>
2024-01-12 01:25:43 -0300

    carousel terminado y listo para fusionar con main tree

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,10 @@ const menuHamburguesa = document.querySelector("#menu-hamburguesa");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 const navLink = document.querySelectorAll(".nav-link");
+//variables para el carousel
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
 
 //funcionalidad de desplazamiento hacia arriba
 scrollUp.addEventListener("click", () => {
@@ -28,3 +32,28 @@ menuHamburguesa.addEventListener("click", () => {
             ul.classList.remove("show");
         })
     });
+
+
+//funcionalidad de carrusel
+function showSlide(index) {
+    carouselInner.style.transform = `translateX(-${index * 100}%)`;
+  }
+  function setActiveClass(index) {
+    carouselItems.forEach((item, i) => {
+      item.classList.toggle('active', i === index);
+    });
+  }
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    showSlide(currentIndex);
+    setActiveClass(currentIndex);
+  }
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    showSlide(currentIndex);
+    setActiveClass(currentIndex);
+  }
+  setActiveClass(currentIndex);
+
+  document.querySelector('#nextBtn').addEventListener('click', nextSlide);
+document.querySelector('#prevBtn').addEventListener('click', prevSlide);
